fefdecdb8d55d038059af0499147ca9c3094f033
FLOREZ Jhon <c227732@chulg>
2025-01-15 15:59:53 +0100

    first commit Vue.js config

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-row items-center gap-5 m-4" >
+    <div>
+      <Logo />
+    </div>
+    <div>
+      <h1> Cicle Méteo </h1>
+    </div>
+    </div>
+</template>
+
+<script>
+
+//importation du composant header
+import Logo from './components/logo.vue';
+
+export default {
+  name: "App",
+  components: {
+    Logo, // Utilise la bonne casse pour le composant
+  }
+}
+</script>
